3afbc5a083c90d37277f5faeecb6c9b70540fa4e
Peter Valdez <peter@nycmesh.net>
2016-12-06 20:51:48 +0700

    Fix a bug where trailing newlines would have iOS returning an extra line

--- ios/RNSSH.m
+++ ios/RNSSH.m
@@ -12,19 +12,24 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(execute:(NSDictionary *)config command:(NSString *)command resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    NMSSHSession *session = [NMSSHSession connectToHost:config[@"host"]
-                                           withUsername:config[@"user"]];
+    // Create the SSH session
+    NMSSHSession *session = [NMSSHSession connectToHost:config[@"host"] withUsername:config[@"user"]];
 
+    // Authenticate
     if (session.isConnected) {
         [session authenticateByPassword:config[@"password"]];
     }
 
+    // Execute the command and disconnect
     NSError *error = nil;
     NSString *response = [session.channel execute:command error:&error];
-
     [session disconnect];
 
+    // Trim starting and ending newlines, then split by them
+    response = [response stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
     NSArray *result = [response componentsSeparatedByString:@"\n"];
+
+    // Return the command output as an array of strings
     resolve(result);
 }
 
